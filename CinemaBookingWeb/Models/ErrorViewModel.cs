009410cddf400ec871c15ab34c41b13c8eb503ac
trankhoinguyen03 <khoinguyen15023@gmail.com>
2024-10-15 14:44:12 +0700

    Update

---
+++ CinemaBookingWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CinemaBookingWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
