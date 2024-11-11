2e8c5ac9c543f69e263d6ddcb0de6308c10e8d4e
thanhphong28 <102896153+thanhphong28@users.noreply.github.com>
2024-11-11 18:19:44 +0700

    sửa lại hết giao diện phần user
    đổi thành boostrap để chỉnh giao diện không bị xung đột

--- CinemaBookingWeb/obj/Debug/net8.0/CinemaBookingWeb.AssemblyInfo.cs
+++ CinemaBookingWeb/obj/Debug/net8.0/CinemaBookingWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaBookingWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23a628e817bc4fee54cbd5a46982ff7851cc64d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d5fa99f873423623cd51ea57ccbde1f8c503495")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaBookingWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaBookingWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
