106b46db2d993914b4019af74b06395a6bc63297
thanhphong28 <102896153+thanhphong28@users.noreply.github.com>
2024-12-07 17:55:45 +0700

    sửa css phần mua vé user

--- CinemaBookingWeb/obj/Debug/net8.0/CinemaBookingWeb.AssemblyInfo.cs
+++ CinemaBookingWeb/obj/Debug/net8.0/CinemaBookingWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaBookingWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7490d5b6d939bb912b23540021eae86eefbb0da8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f399852bb4716a44bb1398c5894b838549d9afd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaBookingWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaBookingWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
